b00b18dbe094b1fa480fb4ab7aa942f69954f439
kono-meowmeow <aya.ayatsuki@gmail.com>
2025-03-29 23:49:23 +0900

    Add additional greeting examples to index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,14 @@ console.log(greeting + expressionTarget); // "Hello, Hello, "
 
 const text: string = greeting + "Bob!";
 console.log(text); // "Hello, Bob!"
+
+let target2: string = "World!";
+target2 = "TypeScript!";
+console.log(greeting + target2); // "Hello, TypeScript!"
+
+let target3: string;
+target3 = "World!";
+console.log(greeting + target3); // "Hello, World!"
+
+// let errorTarget: string;
+// console.log(greeting + errorTarget); // error: Variable 'errorTarget' is used before being assigned
