f476095f6de6e03de300c670ced13290485cd9d5
kono-meowmeow <aya.ayatsuki@gmail.com>
2025-03-29 23:35:34 +0900

    Add additional greeting messages to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 const message: string = "Hello, TypeScript!";
-console.log(message);
+console.log(message); // "Hello, TypeScript!"
+
+const greeting: string = "Hello, ";
+const target: string = "World!";
+console.log(greeting + target); // "Hello, World!"
+
+const expressionTarget: string = greeting;
+console.log(greeting + expressionTarget); // "Hello, Hello, "
+
+const text: string = greeting + "Bob!";
+console.log(text); // "Hello, Bob!"
