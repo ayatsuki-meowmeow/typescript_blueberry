b09bb89887129b4572eb31f16d6c3d213a1091d1
kono-meowmeow <aya.ayatsuki@gmail.com>
2025-04-06 18:03:59 +0900

    Add bigint arithmetic examples to index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,3 +30,11 @@ console.log(area); // 10.5
 
 const million: number = 1_000_000;
 console.log(million); // 1000000
+
+const bignum: bigint = (123n + 456n) * 2n;
+console.log(bignum); // 1158n
+
+const result = 5n / 2n;
+console.log(result); // 2n
+
+// const errorResult = 5 / 2n; // error TS2365: Operator '/' cannot be applied to types 'number' and 'bigint'.
